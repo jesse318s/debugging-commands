178e365ad71cbb4497e9bfaa2ce9c4bcb3d829cb
jesse318s <jesse318s@gmail.com>
2026-03-16 09:53:57 -0500

    change bash file to cmd

---
+++ command_prompt.cmd
@@ -0,0 +1,13 @@
+:: Install ripgrep via winget (upgrades ripgrep if there is an existing installation):
+winget install BurntSushi.ripgrep.MSVC
+
+:: Search a directory for a search term via ripgrep:
+rg "<search_term>"
+
+:: Get my public IP address via curl: 
+curl ifconfig.me
+
+:: Find all processes running on a specific port via netstat, 
+:: and then get the process name for a PID (process id) via tasklist:
+netstat -ano | findstr :<port>
+tasklist /FI "PID eq <PID>"
